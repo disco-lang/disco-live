4e3ac77da616773dc0d46a4c8574c1a01ac38722
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2025-12-11 16:40:34 +0100

    Add interpreter history to user interface

--- src/Main.hs
+++ src/Main.hs
@@ -11,17 +11,23 @@ import Eval (RefRepl, eval, initDisco)
 foreign import javascript unsafe "document.getElementById($1)"
   js_document_getElementById :: JSString -> IO JSVal
 
+foreign import javascript unsafe "document.createElement($1)"
+  js_document_createElement :: JSString -> IO JSVal
+
 foreign import javascript unsafe "$1.addEventListener($2, $3)"
   js_addEventListener :: JSVal -> JSString -> JSVal -> IO ()
+
+foreign import javascript unsafe "$1.appendChild($2)"
+  js_element_appendChild :: JSVal -> JSVal -> IO ()
 
 foreign import javascript "wrapper"
   asEventListener :: (JSVal -> IO ()) -> IO JSVal
 
-foreign import javascript unsafe "$1.target.value"
-  js_event_target_value :: JSVal -> IO Double
-
 foreign import javascript unsafe "$1.value"
   js_input_value :: JSVal -> IO JSString
+
+foreign import javascript unsafe "$1.textContent = $2"
+  js_element_setTextContent :: JSVal -> JSString -> IO ()
 
 foreign import javascript unsafe "view.state.doc.toString()"
   js_view_state_doc_toString :: IO JSString
@@ -41,7 +47,7 @@ foreign export javascript "setup" setup :: IO ()
 setup :: IO ()
 setup = do
     ref <- initDisco
-    
+
     -- Register callback for button click.
     evalButton <- js_document_getElementById (toJSString "eval")
     onEvalButtonCallback <- asEventListener (onEvalButtonClick ref)
@@ -55,5 +61,16 @@ onEvalButtonClick ref event = do
     expr    <- fromJSString <$> js_input_value exprIn
 
     result <- eval ref expr
+    logHistory $ "disco> " <> expr
+    logHistory result
+    {-
     outDiv <- js_document_getElementById (toJSString "out")
     js_element_setInnerHtml outDiv (toJSString result)
+    -}
+
+logHistory :: String -> IO ()
+logHistory s = do
+    div <- js_document_getElementById (toJSString "out")
+    pre <- js_document_createElement (toJSString "pre")
+    js_element_appendChild div pre
+    js_element_setInnerHtml pre (toJSString s)
--- www/js/editor.js
+++ www/js/editor.js
@@ -2,7 +2,7 @@
     'use strict'
     const {basicSetup, EditorView} = CM["codemirror"]
     window.view = new EditorView({
-      doc: "1 + 2",
+      doc: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
       parent: document.querySelector("#editor"),
       extensions: [basicSetup]
     })
