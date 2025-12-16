26c063bd748e269dcf94dda0240a50f99164b5d4
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2025-12-16 12:02:46 +0100

    Add buttons save, load, copy link

--- www/js/editor.js
+++ www/js/editor.js
@@ -1,8 +1,14 @@
 (function() {
     'use strict'
     const {basicSetup, EditorView} = CM["codemirror"]
+
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    let param = urlParams.get('module')
+    if (param === null) { param = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" }
+
     window.view = new EditorView({
-      doc: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+      doc: decodeURIComponent(param),
       parent: document.querySelector("#editor"),
       extensions: [basicSetup]
     })
